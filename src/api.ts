5851251e9f8cd3af6b819c1914b7745c53b51eaa
Sanya Hussain <sanz_133@hotmail.com>
2024-07-09 21:07:30 +0100

    need to use fetch as axios has issues

--- src/api.ts
+++ src/api.ts
@@ -2,6 +2,10 @@ export const getContacts = () => {
   fetch("mongo db when deployed");
 };
 
+export const getContactsByName = () => {
+    fetch("mongo db when deployed");
+  };
+
 export const postContact = () => {
   fetch("mongo db when deployed");
 };
@@ -9,6 +13,10 @@ export const postContact = () => {
 export const getChats = () => {
   fetch("mongo db when deployed");
 };
+
+export const getChatsByName = () => {
+    fetch("mongo db when deployed");
+  };
 
 export const postChats = () => {
   fetch("mongo db when deployed");
