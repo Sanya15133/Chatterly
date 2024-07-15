91de51e01a08d36d236c92ec9461df1d803f93d5
Sanya Hussain <sanz_133@hotmail.com>
2024-07-15 12:00:51 +0100

    fixed chat route

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <nav>
     <router-link to="/">Home</router-link> |
-    <router-link to="/chats">Chats</router-link>
+    <router-link to="/chats/:name">Chats</router-link>
     <router-link to="/contacts">Contacts</router-link>
     <router-link to="/login">Sign In</router-link>
     <router-link to="/portal">Portal</router-link>
