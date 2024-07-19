207aea1ae94663ad99f252542b0a6242372d09c1
Sanya Hussain <sanz_133@hotmail.com>
2024-07-19 10:57:42 +0100

    rendered all messages

--- src/views/MessagesInbox.vue
+++ src/views/MessagesInbox.vue
@@ -4,22 +4,15 @@
         <LoadingComponent v-if="isLoading"/>
         <ErrorComponent v-if="Message" :Status="Status" :Message="Message" />
         <div v-else class="form">
-            <h2></h2>
-            <p></p>
-            <button>
-            View
-            </button>
+            <div v-for="(item, index) in data.chats" :key="index" class="card">
+            <p>{{ item.name }}</p>
+            <p>{{ item.message }}</p>
         </div>
     </div>
+</div>
 </template>
 
 <style>
-img {
-  border: 2px solid grey;
-  border-radius: 10%;
-  justify-content: center;
-  display: inline-block;
-}
 
 .container {
   display: inline-block;
@@ -53,7 +46,7 @@ import { getChats } from '../api'
 type Chat = {
     id: number;
     name: string;
-    Message: string;
+    message: string;
     Date: string
 }
 
