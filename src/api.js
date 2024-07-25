8dfca7df9a5aa68394da24cf18883dec96200779
Sanya Hussain <sanz_133@hotmail.com>
2024-07-25 16:42:36 +0100

    fixed a few bugs

--- src/api.js
+++ src/api.js
@@ -1,88 +1,54 @@
 export const getContacts = async () => {
-    try {
-        const response = await fetch('https://chatterly-be.onrender.com/users');
-        const body = response.json();
-        return body;
-    }
-    catch (error) {
-        throw error;
-    }
+    const response = await fetch('https://chatterly-be.onrender.com/users');
+    const body = response.json();
+    return body;
 };
 export const getContactsByName = async (name) => {
-    try {
-        const response = await fetch(`https://chatterly-be.onrender.com/users/${name}`);
-        const body = await response.json();
-        return body;
-    }
-    catch (error) {
-        throw error;
-    }
+    const response = await fetch(`https://chatterly-be.onrender.com/users/${name}`);
+    const body = await response.json();
+    return body;
 };
 export const postContact = async (name, email, password, avatar) => {
-    try {
-        const response = await fetch('https://chatterly-be.onrender.com/users', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ name, email, password, avatar })
-        });
-        const body = await response.json();
-        return body;
-    }
-    catch (error) {
-        throw error;
-    }
+    const response = await fetch('https://chatterly-be.onrender.com/users', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ name, email, password, avatar })
+    });
+    const body = await response.json();
+    return body;
 };
 export const getChats = async () => {
-    try {
-        const response = await fetch('https://chatterly-be.onrender.com/chats');
-        const body = response.json();
-        return body;
-    }
-    catch (error) {
-        throw error;
-    }
+    const response = await fetch('https://chatterly-be.onrender.com/chats');
+    const body = response.json();
+    return body;
 };
 export const getChatsByName = async (name) => {
-    try {
-        const response = await fetch(`https://chatterly-be.onrender.com/chats/${name}`);
-        const body = response.json();
-        return body;
-    }
-    catch (error) {
-        throw error;
-    }
+    const response = await fetch(`https://chatterly-be.onrender.com/chats/${name}`);
+    const body = response.json();
+    return body;
 };
 export const postChats = async (name, message, date) => {
-    try {
-        const response = await fetch('https://chatterly-be.onrender.com/chats', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ name, message, date })
-        });
-        const body = response.json();
-        return body;
-    }
-    catch (error) {
-        throw error;
-    }
+    const response = await fetch('https://chatterly-be.onrender.com/chats', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ name, message, date })
+    });
+    const body = response.json();
+    console.log(body);
+    return body;
 };
 export const signInContact = async (name, password) => {
-    try {
-        const response = await fetch('https://chatterly-be.onrender.com/users/login', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ name, password })
-        });
-        const body = response.json();
-        return body;
-    }
-    catch (error) {
-        throw error;
-    }
+    const response = await fetch('https://chatterly-be.onrender.com/users/login', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ name, password })
+    });
+    const body = response.json();
+    return body;
 };
